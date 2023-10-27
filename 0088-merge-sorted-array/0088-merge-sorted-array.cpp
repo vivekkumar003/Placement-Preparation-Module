aed1037be5ce0dab409de3de7c8aefb8032c8eca
vivekkumar003 <94454563+vivekkumar003@users.noreply.github.com>
2023-10-27 11:41:36 +0530

    Time: 0 ms (100%), Space: 9.6 MB (11.89%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -6,14 +6,20 @@ public:
         int k=m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i]>nums2[j]){
-                nums1[k--]=nums1[i--];
+                nums1[k]=nums1[i];
+                k--;
+                i--;
             }
             else{
-                 nums1[k--]=nums2[j--];
+                 nums1[k]=nums2[j];
+                 k--;
+                 j--;
             }
         }
         while(j>=0){
-            nums1[k--]=nums2[j--];
+            nums1[k]=nums2[j];
+            k--;
+            j--;
         }
     }
 };
